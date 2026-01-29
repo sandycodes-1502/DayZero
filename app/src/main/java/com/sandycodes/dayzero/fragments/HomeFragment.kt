d31ca20556d03cd24946cb3f6bd9b7000780f877
sandycodes-1502 <torkadsandesh@gmail.com>
2026-01-29 14:40:35 +0530

    Vector Logo and Home Fragment added

--- app/src/main/java/com/sandycodes/dayzero/fragments/HomeFragment.kt
+++ app/src/main/java/com/sandycodes/dayzero/fragments/HomeFragment.kt
@@ -1,10 +1,12 @@
 package com.sandycodes.dayzero.fragments
 
+import android.graphics.drawable.AnimatedVectorDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import com.sandycodes.dayzero.R
 class HomeFragment : Fragment() {
 
@@ -13,6 +15,13 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view =  inflater.inflate(R.layout.fragment_home, container, false)
+
+//        val logo = view.findViewById<ImageView>(R.id.logo)
+
+//        val drawable = logo.drawable
+//        if (drawable is AnimatedVectorDrawable) {
+//            drawable.start()
+//        }
 
         return view
     }
